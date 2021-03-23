fc2fb109fd1bdf5ef900d4388c12ad0d385ada21
Meli <kimberlyoviedo0612@gmail.com>
2021-03-23 08:48:12 -0700

    create game window

---
+++ GameScreen/src/Display.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+public class Display extends JFrame {
+
+    private final int WIDTH = 800, HEIGHT = 600;
+
+    private final int FPS =60;
+    private double TARGETTIME;
+    private double delta =0;
+    private int AVERAGE_FPS = FPS;
+
+
+    public Display(){
+        setTitle(" Space Invaders");
+        setSize(WIDTH,HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Display();
+
+    }
+}
