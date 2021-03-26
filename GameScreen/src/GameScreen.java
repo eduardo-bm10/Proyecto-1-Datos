77c00a8628dae0963b28b5d84eb2697d686411f3
Eduardo <eduardo.bm10@estudiantec.cr>
2021-03-25 19:48:47 -0600

    Crear mas clases

---
+++ GameScreen/src/GameScreen.java
@@ -0,0 +1,7 @@
+public class GameScreen
+{
+    public static void main(String[] args)
+    {
+        Display ventana = new Display();
+    }
+}
---
+++ GameScreen/src/Row.java
@@ -0,0 +1,27 @@
+public class Row
+{
+    public static class BasicRow
+    {
+        int length = 5;
+    }
+    public static class ClassARow extends BasicRow
+    {
+
+    }
+    public static class ClassBRow extends ClassARow
+    {
+
+    }
+    public static class ClassCRow extends ClassARow
+    {
+
+    }
+    public static class ClassDRow extends ClassCRow
+    {
+
+    }
+    public static class ClassERow extends ClassCRow
+    {
+
+    }
+}
