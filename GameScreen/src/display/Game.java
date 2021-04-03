7012c5b6467f1b88fdfa09fbaab0c9f8360b1e9e
Eduardo <eduardo.bm10@estudiantec.cr>
2021-04-03 13:55:41 -0600

    Update BasicRow y crear Node

--- GameScreen/src/display/Game.java
+++ GameScreen/src/display/Game.java
@@ -3,14 +3,10 @@ package display;
 import characters.Player;
 import characters.Enemy;
 
-public class GameScreen
+public class Game
 {
     public static void main(String[] args)
     {
         Display ventana = new Display();
-
-        Player jugador = new Player(3);
-
-        ventana.add(jugador);
     }
 }
