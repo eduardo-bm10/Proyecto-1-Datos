3f747a3fd2e7a8c9e8b14ae8c1707aeebdf62c7f
Eduardo <eduardo.bm10@estudiantec.cr>
2021-04-06 21:51:18 -0600

    Crear jugador con rectangulo

--- GameScreen/src/characters/Player.java
+++ GameScreen/src/characters/Player.java
@@ -1,22 +1,28 @@
 package characters;
 
-import images.ImageLoader;
 import javax.swing.JPanel;
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
+import java.awt.geom.Rectangle2D;
 
 public class Player extends JPanel
 {
-    public ImageLoader imagen = new ImageLoader(350, 400);
+    public Rectangle2D.Float jugador = new Rectangle2D.Float(350, 400, 50, 70);
     Movement movimiento = new Movement();
 
     public Player()
     {
         addMouseMotionListener(movimiento);
         addMouseListener(movimiento);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2D = (Graphics2D) g;
+        g2D.setColor(Color.RED);
+        g2D.fill(jugador);
     }
 
     class Movement extends MouseAdapter
@@ -29,11 +35,15 @@ public class Player extends JPanel
             int dx = e.getX() - x;
             int dy = e.getY() - y;
 
-            if (imagen.contains(x, y))
+            if (jugador.getBounds2D().contains(x, y))
             {
-                imagen.posicionX += dx;
-                imagen.posicionY += dy;
-                imagen.repaint();
+                jugador.x += dx;
+                jugador.y += dy;
+                repaint();
+            }
+            else{
+                jugador.x = e.getX() - 40;
+                jugador.y = e.getY() - 50;
             }
             x += dx;
             y += dy;
--- GameScreen/src/images/ImageLoader.java
+++ GameScreen/src/images/ImageLoader.java
@@ -1,27 +1,16 @@
 package images;
 
-import characters.Player;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
 
 public class ImageLoader extends JPanel
 {
-    public int posicionX;
-    public int posicionY;
+    public int x;
+    public int y;
+    public int width;
+    public int height;
 
-    public ImageLoader(int x, int y)
-    {
-        this.posicionX = x;
-        this.posicionY = y;
-    }
-    public void paint(Graphics g)
-    {
-        super.paint(g);
-        Toolkit t = Toolkit.getDefaultToolkit();
-        Image i = t.getImage("GameScreen/images/player.png");
-        Graphics2D g2D = (Graphics2D) g;
-        g2D.drawImage(i, posicionX, posicionY, this);
-    }
+
 }
 
